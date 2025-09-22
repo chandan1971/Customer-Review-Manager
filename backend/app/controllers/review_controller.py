1e0a625280f4ffacde865f63c6b90c42a76f6a69
Chandan Dey <chandandey1357@gmail.com>
2025-09-22 23:50:26 +0530

    Added filter api

--- backend/app/controllers/review_controller.py
+++ backend/app/controllers/review_controller.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from fastapi import HTTPException
 from app.schemas.review_dao import ReviewDAO
 from app.services.review_service import ReviewService
+from app.schemas.review_filter_request_dto import ReviewFilterRequest
 
 class ReviewController:
     def __init__(self, db: Session):
@@ -22,3 +23,11 @@ class ReviewController:
             return {"review": review}
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
+        
+    def get_reviews(self, filters: ReviewFilterRequest):
+        if filters.page <= 0:
+            raise HTTPException(status_code=400, detail="Page must be >= 1")
+        if filters.page_size <= 0 or filters.page_size > 100:
+            raise HTTPException(status_code=400, detail="Page size must be between 1 and 100")
+        
+        return self.service.get_reviews(filters)
--- backend/app/services/review_service.py
+++ backend/app/services/review_service.py
@@ -5,6 +5,11 @@ from app.utils.db_utils import insert_reviews_bulk
 from app.utils.logger import get_logger
 from app.utils.db_utils import dao_to_dto
 from app.utils.db_utils import get_review_by_id
+from app.schemas.review_model import ReviewModel
+from sqlalchemy import or_
+from app.schemas.review_filter_request_dto import ReviewFilterRequest
+from app.utils.filter_util import normalize_filter
+
 logger = get_logger(__name__)
 
 class ReviewService:
@@ -21,4 +26,38 @@ class ReviewService:
     
     def get_review_by_id(self, id:int):
         return get_review_by_id(self.db,id)
+    
+    def get_reviews(self, filters: ReviewFilterRequest):
+        query = self.db.query(ReviewModel)
+        location = normalize_filter(filters.location)
+        sentiment = normalize_filter(filters.sentiment)
+        q = normalize_filter(filters.q)
+
+        
+        if location:
+            query = query.filter(ReviewModel.location.ilike(f"%{location}%"))
+
+        if sentiment :
+            query = query.filter(ReviewModel.review_text.ilike(f"%{sentiment}%"))
+
+        if q:
+            or_filters = []
+            if q:
+                or_filters.append(ReviewModel.review_text.ilike(f"%{q}%"))
+                or_filters.append(ReviewModel.location.ilike(f"%{q}%"))
+
+            if or_filters:
+                query = query.filter(or_(*or_filters))
+
+        
+        total = query.count()
+        results = query.offset((filters.page - 1) * filters.page_size).limit(filters.page_size).all()
+
+        
+        return {
+            "total": total,
+            "page": filters.page,
+            "page_size": filters.page_size,
+            "reviews": results
+        }
 
