ebe48d385b6c8bdfab970185e6e6c96bf193516e
Chandan Dey <chandandey1357@gmail.com>
2025-09-23 15:15:39 +0530

    Added suggest reply method and solved alternate failing queries

--- backend/app/connections/postgres_db.py
+++ backend/app/connections/postgres_db.py
@@ -1,13 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from app.config import settings
+from sqlalchemy.pool import NullPool
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    settings.DATABASE_URL,
+    poolclass=NullPool,     
+    pool_pre_ping=True,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
     db: Session = SessionLocal()
     try:
         yield db
+        db.commit()  
+    except:
+        db.rollback()  
+        raise
     finally:
-        db.close()
+        db.close()   
--- backend/app/utils/db_utils.py
+++ backend/app/utils/db_utils.py
@@ -6,6 +6,7 @@ from app.schemas.review_model import ReviewModel
 from app.utils.logger import get_logger
 from typing import List
 from fastapi import HTTPException
+from sqlalchemy.exc import SQLAlchemyError
 
 logger = get_logger(__name__)
 
@@ -18,38 +19,33 @@ def dao_to_dto(dao: ReviewDAO) -> ReviewCreateDTO:
         review_date=dao.date
     )
 
-
-
 def insert_reviews_bulk(db: Session, reviews: List[ReviewCreateDTO]) -> int:
     if not reviews:
+        logger.info("No reviews provided for bulk insert")
         return 0
     
     review_dicts = [review.model_dump() for review in reviews]
 
     try:
-        db.bulk_insert_mappings(
-            ReviewModel,  
-            review_dicts
-        )
-        db.commit()
+        db.bulk_insert_mappings(ReviewModel, review_dicts)
+        db.flush()  
+        logger.info(f"Prepared {len(reviews)} reviews for insert")
         return len(reviews)
     except Exception as e:
-        db.rollback()
-        logger.error("Failed to insert reviews bulk", e)
-        return 0
+        logger.error("Failed to insert reviews bulk", exc_info=True)
+        raise
+  
 
 def get_review_query(id: int):
     return text("SELECT * FROM reviews WHERE id = :id").bindparams(id=id)
 
-
 def get_review_by_id(db: Session, id: int):
     query = get_review_query(id)
     try:
-        result = db.execute(query).mappings().first()  
+        result = db.execute(query).mappings().first()
         if not result:
             raise HTTPException(status_code=404, detail=f"Review with ID {id} not found")
+        return result
     except Exception as e:
-        raise e
-    
-    
-    return result
+        logger.error(f"Error fetching review with ID {id}", exc_info=True)
+        raise
