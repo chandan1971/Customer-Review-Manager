87c177157e45132cce88fcd2f7c7e03d0d3e3e27
Chandan Dey <chandandey1357@gmail.com>
2025-09-25 19:26:05 +0530

    Corrected db connection setting

--- backend/app/connections/postgres_db.py
+++ backend/app/connections/postgres_db.py
@@ -2,11 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from app.config import settings
 from sqlalchemy.pool import NullPool
+import ssl
+
+
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = True
+ssl_context.verify_mode = ssl.CERT_REQUIRED
 
 engine = create_engine(
     settings.DATABASE_URL,
     poolclass=NullPool,     
     pool_pre_ping=True,
+    connect_args={"ssl_context": ssl_context}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
